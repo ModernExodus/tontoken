71d445ab772961e80dd3e7b8b0abe84940c551c6
John Washburn <johnlwashburn@gmail.com>
2021-08-04 22:17:15 -0400

    changed key generator generate functions to internal

--- contracts/UniqueKeyGeneratorProxy.sol
+++ contracts/UniqueKeyGeneratorProxy.sol
@@ -5,6 +5,15 @@ import "./UniqueKeyGenerator.sol";
 
 // proxy to interact with UniqueKeyGenerator for unit testing
 contract UniqueKeyGeneratorProxy is UniqueKeyGenerator {
+
+    function generateKeyP(address a) public view returns (bytes32) {
+        return super.generateKey(a);
+    }
+
+    function generateKeyP(uint256 u) public view returns (bytes32) {
+        return super.generateKey(u);
+    }
+
     function changeSalt() public {
         super.addSalt();
     }
